ed7cd760b6e39698a1dc3a3eb08a6c33b8a9fb27
Richard Rothwell <rrothwell@v3.org.au>
2014-06-04 15:34:10 +1000

    RR: added point hand for cursor.

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 # 0. Login to the on remote server.
 # 1. clone the git repo. $ git pull hierarchal_pie
 # 2. navigate into the working copy. $ cd ncr002-dataanalysis
-# 3. Run this script $ ./install.sh
+# 3. Run this script $ sudo ./install.sh
 # 4. if necessary create some .htaccess files, define some Apache passwords and edit httpd.conf.
 
 sudo cp ./distribution/nectar_visualisation.tgz /var/www
 cd /var/www
 sudo tar -xvf nectar_visualisation.tgz
-sudo cd nectar_visualisation/
+cd nectar_visualisation/
 sudo rm static/js/._*
 sudo rm static/img/._*
 sudo rm static/css/._*
@@ -18,3 +18,4 @@ sudo rm static/._*
 sudo rm data/._*
 sudo rm media/._*
 sudo rm scripts/._*
+sudo rm nectar_visualisation.tgz
